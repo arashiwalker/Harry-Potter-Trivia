67505e1f79469614022ecb0afb901ddca34e29bc
arashi <arashiwalker@gmail.com>
2019-07-23 18:37:57 -0700

    adeed some styling ing images for quiz answers and object for js code questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,45 @@
+var card = $("#quiz-area");
+var countStartNumber = 30;
+
+var questions = [{
+  question: "Harry Potter joined what house?",
+  answers: ["Slytherin", "Gryffindor", "Hufflepuff", "Raveclaw"],
+  correctAnswer: "Gryffindor",
+  image: "assets/images/gryffindor.gif"
+}, {
+  question: "Who is Fluffy?",
+  answers: ["Hermonie's cat", "Harry's owl", "A three headed dog", "Hagrids dragon"],
+  correctAnswer: "A three headed dog",
+  image: "assets/images/fluffy.gif"
+}, {
+  question: "What position does Harry Potter play on his quidditch team?",
+  answers: ["Chaser", "Seeker", "Keeper", "Bludger"],
+  correctAnswer: "Seeker",
+  image: "assets/images/seeker.gif"
+}, {
+  question: "What is Harry's patronous?",
+  answers: ["Stag", "Owl", "Rabbit", "Unicorn"],
+  correctAnswer: "Stag",
+  image: "assets/images/Stag.gif"
+}, {
+  question: "How did moaning Myrtle die?",
+  answers: ["A mountain troll", "The Whomping Willow", "The Basilisk", "The Killing Curse"],
+  correctAnswer: "The Basilisk",
+  image: "assets/images/basilisk.gif"
+}, {
+  question: "What type of dragon did Harry face in his first Tri-Wizard Tournament task",
+  answers: ["Hungarian Horntail", "Chinese Fireball", "Swedish Shortsnout", "Welsh Green"],
+  correctAnswer: "Hungarian Horntail",
+  image: "assets/images/dragon.gif"
+}, {
+  question: "What does the spell Incendio do?",
+  answers: ["Creates fire", "Hide memories", "Opens doors", "Vanish objects"],
+  correctAnswer: "Creates fire",
+  image: "assets/images/spells.gif"
+}, {
+  question: 'who is reffered to such expressions as "You-Know-Who", "He Who Must Not Be Named" or "the Dark Lord"?',
+  answers: ["Ron Weasley", "Dumbledore", "Draco Malfoy", "Lord Voldemort"],
+  correctAnswer: "Lord Voldemort",
+  image: "assets/images/voldemort.gif"
+}];
+
